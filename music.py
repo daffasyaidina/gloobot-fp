4c7b7cb5ab10eab8f1383a29ef240886cf4e59b7
daffasyaidina <daffasyaidina89@gmail.com>
2021-12-30 17:24:35 +0700

    Added main.py and music.py

---
+++ music.py
@@ -0,0 +1,48 @@
+import discord
+from discord.ext import commands
+import youtube_dl
+
+
+class music(commands.Cog):
+    def __init__(self, client):
+        self.bot = client
+
+    @commands.command()
+    async def join(self, ctx):
+        if ctx.author.voice is None:
+            await ctx.send("Bro, you're not in the channel")
+        voice_channel = ctx.author.voice.channel
+        if ctx.voice_client is None:
+             await voice_channel.connect()
+        else:
+             await ctx.voice_client.move_to(voice_channel)
+    
+    @commands.command()
+    async def disconnect(self, ctx):
+        await ctx.voice_client.disconnect()
+
+    @commands.command()
+    async def play(self, ctx, url):
+        ctx.voice_client.stop()
+        FFMPEG_OPTIONS = {'before_options': '-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5', 'options': 'vn'}
+        vc = ctx.voice_client
+
+        with youtube_dl.YoutubeDL() as ydl:
+            info = ydl.extract_info(url, download = False)
+            url2 = info['formats'][0]['url']
+            source = await discord.FFmpegOpusAudio.from_probe(url2, **FFMPEG_OPTIONS) #string to play audio
+            vc.play(source)
+
+    @commands.command()
+    async def pause(self, ctx):
+        ctx.voice_client.pause()
+        await ctx.send("Paused music")
+    
+    @commands.command()
+    async def resume(self, ctx):
+        ctx.voice_client.resume()
+        await ctx.send("Resume music")
+
+
+def setup(client):
+    client.add_cog((music)(client))
